63a965e40bb6a21828f54c5a1b179c08648eb1a9
Beknasar <680633@gmail.com>
2023-04-08 00:13:17 +0300

    Added ranking system to home page

--- main.py
+++ main.py
@@ -54,7 +54,14 @@ class FindMovieForm(FlaskForm):
 # Routes
 @app.route("/")
 def home():
-    all_movies = db.session.query(Movie).all()
+    # This line creates a list of all the movies sorted by rating
+    all_movies = db.session.query(Movie).order_by(Movie.rating).all()
+
+    # This line loops through all the movies
+    for i in range(len(all_movies)):
+        # This line gives each movie a new ranking reversed from their order in all_movies
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
     return render_template("index.html", movies=all_movies)
 
 
